caef5ce4661d48594c5440d46b6ca1564a6dabf4
“043ali” <alirehman41f@gmail.com>
2024-01-05 18:28:42 +0800

    checking

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -21,7 +21,7 @@ export default function EditInvoiceForm({
 }) {
 
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
+ // checking
   return (
     <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
